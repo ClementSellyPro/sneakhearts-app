6fe2a4ef246c6094ac01e96cc4c74abe16c4fe66
ClementSellyPro <clementselly.pro@gmail.com>
2025-08-26 15:15:54 +0400

    Little restructure of the file structure for best practice

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { User, Heart, ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between px-12 bg-white w-full">
-      <Image
-        src="/logo/logo_sneakhearts.svg"
-        width={60}
-        height={60}
-        alt="Logo"
-        priority
-        className="cursor-pointer"
-      />
+      <Link href={"/"}>
+        <Image
+          src="/logo/logo_sneakhearts.svg"
+          width={60}
+          height={60}
+          alt="Logo"
+          priority
+          className="cursor-pointer"
+        />
+      </Link>
       <nav>
         <ul className="flex gap-12 text-xl font-medium">
           <li className="nav-item relative">
