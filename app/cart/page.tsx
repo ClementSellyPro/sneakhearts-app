797bff30973d8b064b788c0558903b9afa781127
ClementSellyPro <clementselly.pro@gmail.com>
2025-09-02 17:41:04 +0400

    Add style to cart page when user is not connected

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,46 @@
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+
 export default function Cart() {
   return (
-    <div>
-      <p>yes</p>
+    <div className="flex justify-between h-screen px-52 py-18">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-xl font-semibold">Panier</h1>
+        <p>Il n&apos;y a aucun article dans ton panier.</p>
+        <p>
+          <Link href={"/login"} className="font-semibold hover:underline">
+            Se connecter
+          </Link>{" "}
+          ou{" "}
+          <Link href={"/register"} className="font-semibold hover:underline">
+            créer un compte
+          </Link>{" "}
+          pour ajouter des articles.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-8 w-2/6">
+        <h2 className="text-xl font-semibold">Récapitulatif</h2>
+
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
+            <p>Sous-total</p>
+            <p>---</p>
+          </div>
+
+          <div className="flex justify-between">
+            <p>Frais estimes de prise en charge et d&apos;expédition.</p>
+            <p>Gratuit</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between py-4 border-t border-b">
+          <p className="font-semibold">Total</p>
+          <p>---</p>
+        </div>
+
+        <Button>Paiement</Button>
+      </div>
     </div>
   );
 }
