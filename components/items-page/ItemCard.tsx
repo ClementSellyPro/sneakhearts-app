fbe4f95341d06e71a0fd6870d584691b50117222
ClementSellyPro <clementselly.pro@gmail.com>
2025-09-15 08:56:46 +0400

    Add some style to ItemCard to show products in sales (promotion)

--- components/items-page/ItemCard.tsx
+++ components/items-page/ItemCard.tsx
@@ -27,7 +27,7 @@ export default function ItemCard({ product, category }: ItemCardProps) {
       className="group w-fit pb-8 cursor-pointer relative"
       onMouseLeave={() => setCurrentImage(0)}
     >
-      <div className="border border-gray-300 group-hover:rounded-lg overflow-hidden transition-all">
+      <div className="relative border border-gray-300 group-hover:rounded-lg overflow-hidden transition-all">
         <Image
           src={product.variations[currentImage].largeUrl}
           alt="Sneakers Photo"
@@ -35,6 +35,11 @@ export default function ItemCard({ product, category }: ItemCardProps) {
           height={340}
           className="group-hover:scale-110 transition-all duration-500"
         />
+        {product.variations[currentImage].salePrice ? (
+          <div className="absolute top-2 right-2 bg-red-500 text-white text-sm rounded-full py-0.5 px-3">
+            promotion
+          </div>
+        ) : null}
       </div>
       <div className="flex flex-col gap-2 pt-4 group-hover:pt-1 group-hover:absolute bottom-2 group-hover:z-20 bg-white w-full">
         <p className="text-sm text-gray-400 group-hover:hidden">
@@ -52,7 +57,17 @@ export default function ItemCard({ product, category }: ItemCardProps) {
             {product.name.slice(0, 30)}
             {product.name.length > 30 ? "..." : null}
           </p>
-          <p>
+          <p
+            className={`${
+              product.variations[currentImage].salePrice ? "text-red-500" : null
+            }`}
+          >
+            {product.variations[currentImage].salePrice ? (
+              <span className="text-black font-light line-through">
+                {" "}
+                {product.variations[currentImage].price}{" "}
+              </span>
+            ) : null}
             {product.variations[currentImage].salePrice
               ? product.variations[currentImage].salePrice
               : product.variations[currentImage].price}
