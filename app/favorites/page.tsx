9a43b3c1e03d767e6d3b5b49d0cb550dd241cf0d
ClementSellyPro <clementselly.pro@gmail.com>
2025-09-21 08:28:44 +0400

    Add some style to Favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,47 @@
+import Link from "next/link";
+
 export default function FavoritesPage() {
   return (
-    <div className="p-20">
-      <p>fav</p>
+    <div className="flex justify-between h-screen px-52 py-18">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-xl font-semibold">Articles Favoris</h1>
+        <p>Il n&apos;y a aucun article ajoute comme favoris pour le moment.</p>
+
+        <p>
+          <Link href={"/login"} className="font-semibold hover:underline">
+            Se connecter
+          </Link>{" "}
+          ou{" "}
+          <Link href={"/register"} className="font-semibold hover:underline">
+            créer un compte
+          </Link>{" "}
+          pour ajouter des articles.
+        </p>
+
+        <div className="flex gap-4 flex-wrap">
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+            ITEM
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
