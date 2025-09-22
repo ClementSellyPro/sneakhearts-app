7534a99e2809ff01cac2a9d39a30d213ac61a3b6
ClementSellyPro <clementselly.pro@gmail.com>
2025-09-22 10:33:37 +0400

    Add condition to display the message to connect if user is not connected or hide the message if connected - for Favorites page and Cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,22 +1,32 @@
+"use client";
+
 import Button from "@/components/ui/Button";
+import { useSession } from "@/lib/auth-client";
 import Link from "next/link";
 
 export default function Cart() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex justify-between h-screen px-52 py-18">
       <div className="flex flex-col gap-4">
         <h1 className="text-xl font-semibold">Panier</h1>
         <p>Il n&apos;y a aucun article dans ton panier.</p>
-        <p>
-          <Link href={"/login"} className="font-semibold hover:underline">
-            Se connecter
-          </Link>{" "}
-          ou{" "}
-          <Link href={"/register"} className="font-semibold hover:underline">
-            créer un compte
-          </Link>{" "}
-          pour ajouter des articles.
-        </p>
+
+        {!session?.user ? (
+          <p>
+            <Link href={"/login"} className="font-semibold hover:underline">
+              Se connecter
+            </Link>{" "}
+            ou{" "}
+            <Link href={"/register"} className="font-semibold hover:underline">
+              créer un compte
+            </Link>{" "}
+            pour ajouter des articles.
+          </p>
+        ) : (
+          <p>Liste des produits</p>
+        )}
       </div>
 
       <div className="flex flex-col gap-8 w-2/6">
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,46 +1,53 @@
+"use client";
+
+import { useSession } from "@/lib/auth-client";
 import Link from "next/link";
 
 export default function FavoritesPage() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex justify-between h-screen px-52 py-18">
       <div className="flex flex-col gap-4">
         <h1 className="text-xl font-semibold">Articles Favoris</h1>
         <p>Il n&apos;y a aucun article ajoute comme favoris pour le moment.</p>
 
-        <p>
-          <Link href={"/login"} className="font-semibold hover:underline">
-            Se connecter
-          </Link>{" "}
-          ou{" "}
-          <Link href={"/register"} className="font-semibold hover:underline">
-            créer un compte
-          </Link>{" "}
-          pour ajouter des articles.
-        </p>
-
-        <div className="flex gap-4 flex-wrap">
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
+        {!session?.user ? (
+          <p>
+            <Link href={"/login"} className="font-semibold hover:underline">
+              Se connecter
+            </Link>{" "}
+            ou{" "}
+            <Link href={"/register"} className="font-semibold hover:underline">
+              créer un compte
+            </Link>{" "}
+            pour ajouter des articles.
+          </p>
+        ) : (
+          <div className="flex gap-4 flex-wrap">
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
+            <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
+              ITEM
+            </div>
           </div>
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
-          </div>
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
-          </div>
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
-          </div>
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
-          </div>
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
-          </div>
-          <div className="flex justify-center items-center border rounded-xl w-[250px] h-[300px] text-gray-200 border-gray-500">
-            ITEM
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
