c38eb41d77b4b37a7a5a356ed2aa112a158e8cdd
ClementSellyPro <clementselly.pro@gmail.com>
2025-09-30 09:06:00 +0400

    Fix - FavoritesPage - Do not redirect on the product page if user delete an item

--- components/favorites/FavoriteCard.tsx
+++ components/favorites/FavoriteCard.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { FavoriteProduct } from "@/model/FavoriteType";
 import Image from "next/image";
 import { Trash2 } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface FavoriteCardProps {
   favorite: FavoriteProduct;
@@ -12,31 +15,44 @@ export default function FavoriteCard({
   favorite,
   onDeleteFavItem,
 }: FavoriteCardProps) {
-  return (
-    <Link href={`/product/${favorite.productId}`}>
-      <div className="relative flex gap-4 items-start pr-4 border rounded-xl  text-gray-200 border-gray-300 overflow-hidden cursor-pointer hover:border-gray-500">
-        <Image
-          src={favorite.largeImage}
-          alt={favorite.category}
-          width={150}
-          height={150}
-        />
-        <div className="flex flex-col pt-4 text-black">
-          <span className="text-lg font-semibold">{favorite.name}</span>
-          <span className="text-sm text-gray-500">
-            {favorite.category === "Shoes" ? "Chaussure" : "Vêtement"}
-          </span>
-          <span className="font-semibold">{favorite.currentPrice}</span>
+  const router = useRouter();
 
-          <button
-            className="absolute bottom-2 right-2 w-fit px-4 text-sm border rounded-full border-red-500 hover:text-red-500"
-            type="button"
-            onClick={() => onDeleteFavItem(favorite.id)}
-          >
-            <Trash2 className="w-4" />
-          </button>
-        </div>
+  function handleRedirection(e: React.MouseEvent<HTMLElement>) {
+    const target = e.target as HTMLElement;
+    console.log(target);
+    if (!target.classList.contains("button-delete")) {
+      router.push(`/product/${favorite.productId}`);
+    }
+  }
+
+  return (
+    // <Link href={`/product/${favorite.productId}`}>
+    <div
+      onClick={(e: React.MouseEvent<HTMLElement>) => handleRedirection(e)}
+      className="relative flex gap-4 items-start pr-4 border rounded-xl  text-gray-200 border-gray-300 overflow-hidden cursor-pointer hover:border-gray-500"
+    >
+      <Image
+        src={favorite.largeImage}
+        alt={favorite.category}
+        width={150}
+        height={150}
+      />
+      <div className="flex flex-col pt-4 text-black">
+        <span className="text-lg font-semibold">{favorite.name}</span>
+        <span className="text-sm text-gray-500">
+          {favorite.category === "Shoes" ? "Chaussure" : "Vêtement"}
+        </span>
+        <span className="font-semibold">{favorite.currentPrice}</span>
+
+        <button
+          className="button-delete absolute bottom-2 right-2 w-fit px-4 text-sm border rounded-full border-red-500 hover:text-red-500"
+          type="button"
+          onClick={() => onDeleteFavItem(favorite.id)}
+        >
+          <Trash2 className="button-delete w-4" />
+        </button>
       </div>
-    </Link>
+    </div>
+    // </Link>
   );
 }
