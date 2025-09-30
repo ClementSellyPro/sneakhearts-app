c6058ed079482724f991e267cd420e8a4c471f33
ClementSellyPro <clementselly.pro@gmail.com>
2025-09-30 09:52:43 +0400

    Fix - CartItem - Do not redirect to the product page if user delete an item

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import { CartItemType } from "@/model/CarItemType";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface CartItemProps {
   cartItemData: CartItemType;
 }
 
 export default function CartItem({ cartItemData }: CartItemProps) {
+  const router = useRouter();
+
   async function onDeleteCartItem() {
     try {
       const response = await fetch("/api/cart", {
@@ -26,35 +30,42 @@ export default function CartItem({ cartItemData }: CartItemProps) {
     }
   }
 
-  return (
-    <Link href={`product/${cartItemData.product.variationId}`}>
-      <div className="flex border-b border-gray-300 pb-4">
-        <Image
-          src={cartItemData.product.image}
-          alt={cartItemData.product.name}
-          width={100}
-          height={100}
-          className="rounded-xl cursor-pointer hover:opacity-80"
-        />
-        <div className="p-4 flex-1 cursor-pointer hover:opacity-80">
-          <p className="font-semibold">{cartItemData.product.name}</p>
-          <p className="text-sm text-gray-500">{cartItemData.product.brand}</p>
-        </div>
-        <div className="flex justify-center items-center font-semibold">
-          <p className="p-4">{cartItemData.currentPrice}$</p>
-        </div>
+  function handleRedirection(e: React.MouseEvent<HTMLElement>) {
+    const target = e.target as HTMLElement;
+    if (!target.classList.contains("button-delete"))
+      router.push(`product/${cartItemData.product.variationId}`);
+  }
 
-        <div className="flex justify-center items-center font-semibold p-4">
-          <Image
-            src={"/icon/cross.svg"}
-            alt="Retirer"
-            width={15}
-            height={15}
-            className="cursor-pointer hover:opacity-60"
-            onClick={onDeleteCartItem}
-          />
-        </div>
+  return (
+    <div
+      onClick={(e: React.MouseEvent<HTMLElement>) => handleRedirection(e)}
+      className="flex border-b border-gray-300 pb-4"
+    >
+      <Image
+        src={cartItemData.product.image}
+        alt={cartItemData.product.name}
+        width={100}
+        height={100}
+        className="rounded-xl cursor-pointer hover:opacity-80"
+      />
+      <div className="p-4 flex-1 cursor-pointer hover:opacity-80">
+        <p className="font-semibold">{cartItemData.product.name}</p>
+        <p className="text-sm text-gray-500">{cartItemData.product.brand}</p>
       </div>
-    </Link>
+      <div className="flex justify-center items-center font-semibold">
+        <p className="p-4">{cartItemData.currentPrice}$</p>
+      </div>
+
+      <div className="button-delete flex justify-center items-center font-semibold p-4">
+        <Image
+          src={"/icon/cross.svg"}
+          alt="Retirer"
+          width={15}
+          height={15}
+          className="button-delete cursor-pointer hover:opacity-60"
+          onClick={onDeleteCartItem}
+        />
+      </div>
+    </div>
   );
 }
