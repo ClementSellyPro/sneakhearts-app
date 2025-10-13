79c671cec5ec598ca777b0964c1c827f7800a336
ClementSellyPro <clementselly.pro@gmail.com>
2025-10-13 16:12:48 +0400

    Arrange Footer for mobile screen

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white w-full px-14">
-      <div className="flex items-start justify-between pt-8 pb-14 w-full">
-        <div className="flex gap-32">
+    <footer className="bg-black text-white w-full px-6 md:px-14">
+      <div className="flex flex-col md:flex-row items-start justify-between pt-8 pb-4 md:pb-14 w-full">
+        <div className="flex flex-wrap md:flex-row gap-8 md:gap-32 pb-8 md:pb-0">
           <div className="footer-group">
             <h2 className="footer-title">PRODUITS</h2>
             <ul className="footer-items">
@@ -37,7 +37,7 @@ export default function Footer() {
           style={{ fill: "#fff" }}
         />
       </div>
-      <div className="flex justify-between w-full border-t pt-2 pb-8 text-xs">
+      <div className="flex flex-col md:flex-row gap-4 justify-between w-full border-t pt-2 pb-8 text-xs">
         <div>2025 Sneakhearts (marque fictif)</div>
         <div>
           <ul className="flex gap-4">
