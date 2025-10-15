224254a818390b268af8b251cc6b3ccba650a210
ClementSellyPro <clementselly.pro@gmail.com>
2025-10-15 10:05:51 +0400

    Adapt Highlight component for mobile screen and link the button to Chaussure page

--- components/home/Highlight.tsx
+++ components/home/Highlight.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 import Button from "../ui/Button";
+import Link from "next/link";
 
 export default function Highlight() {
   return (
-    <div className="relative flex items-center justify-between h-screen w-full text-white">
+    <div className="relative flex flex-col md:flex-row items-center justify-between h-screen w-full text-white">
       <div className="flex justify-center items-center h-screen w-1/2">
         <Image
           src={"/sneakers/nike_classic_two.jpg"}
           alt="Sneakers"
           width={453}
           height={800}
-          className="object-cover"
+          className="object-cover h-3/4 w-3/4"
         />
         <Image
           src={"/logo/logo_sneakhearts_simple.svg"}
           alt="Logo sneakhearts"
           width={38}
           height={134}
-          className="absolute bottom-6 left-8 z-10"
+          className="absolute top-6 md:top-auto md:bottom-6 left-8 z-10 w-6 md:w-9"
         />
       </div>
-      <div className="relative p-12 h-screen w-1/2 bg-[#C5B18D]">
-        <h2 className="text-6xl font-semibold">La Classique</h2>
-        <div className="absolute flex flex-col items-start gap-8 bottom-12 text-4xl w-5/6">
+      <div className="relative p-6 md:p-12 h-screen w-full md:w-1/2 bg-[#C5B18D]">
+        <h2 className="text-3xl md:text-6xl font-semibold">La Classique</h2>
+        <div className="absolute flex flex-col items-start gap-8 bottom-12 text-2xl md:text-4xl w-5/6">
           <div>
             <p>
               Vous êtes à la recherche d&apos;une classique pour tous les jours
@@ -31,7 +32,9 @@ export default function Highlight() {
             <p className="mb-10">Décourvrez notre sélection.</p>
           </div>
 
-          <Button>Découvrir</Button>
+          <Link href={"/chaussures"}>
+            <Button>Découvrir</Button>
+          </Link>
         </div>
       </div>
     </div>
