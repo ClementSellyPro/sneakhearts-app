95ef974ca77b75fadce9ac54577382e7fc02ec37
ClementSellyPro <clementselly.pro@gmail.com>
2025-10-16 14:03:26 +0400

    Fix Highlight Component to keep a good ratio and enough space for each part for mobile screen

--- components/home/Highlight.tsx
+++ components/home/Highlight.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function Highlight() {
   return (
     <div className="relative flex flex-col md:flex-row items-center justify-between h-screen w-full text-white">
-      <div className="flex justify-center items-center h-screen w-1/2">
+      <div className="flex justify-center items-center h-1/2 md:h-screen w-1/2 overflow-hidden">
         <Image
           src={"/sneakers/nike_classic_two.jpg"}
           alt="Sneakers"
           width={453}
           height={800}
-          className="object-cover h-3/4 w-3/4"
+          className="object-cover"
         />
         <Image
           src={"/logo/logo_sneakhearts_simple.svg"}
@@ -21,7 +21,7 @@ export default function Highlight() {
           className="absolute top-6 md:top-auto md:bottom-6 left-8 z-10 w-6 md:w-9"
         />
       </div>
-      <div className="relative p-6 md:p-12 h-screen w-full md:w-1/2 bg-[#C5B18D]">
+      <div className="relative p-6 md:p-12 h-1/2 md:h-screen w-full md:w-1/2 bg-[#C5B18D]">
         <h2 className="text-3xl md:text-6xl font-semibold">La Classique</h2>
         <div className="absolute flex flex-col items-start gap-8 bottom-12 text-2xl md:text-4xl w-5/6">
           <div>
