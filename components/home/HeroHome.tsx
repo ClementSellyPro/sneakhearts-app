4f1d02a439fa2249d52e13e61cb52fe7b32b73a7
ClementSellyPro <clementselly.pro@gmail.com>
2025-10-16 10:36:38 +0400

    Adapt HeroHome component for mobile screen and change the video

--- components/home/HeroHome.tsx
+++ components/home/HeroHome.tsx
@@ -17,15 +17,17 @@ export default function HeroHome() {
         loop
         muted
         playsInline
-        className="absolute inset-0 w-full h-full object-cover brightness-80"
+        className="absolute inset-0 w-full h-full object-cover brightness-70 -py-10"
       >
         <source src="/video/background.webm" type="video/webm" />
         <source src="/video/background.mp4" type="video/mp4" />
       </video>
 
-      <div className="absolute left-14 bottom-[20%] z-10 text-white">
-        <p className="text-8xl font-bold">Sneakhearts</p>
-        <p className="text-xl">Chosis ton style. Choisis Sneakhearts.</p>
+      <div className="absolute left-6 md:left-14 bottom-[20%] z-10 text-white">
+        <p className="text-6xl md:text-8xl font-bold">Sneakhearts</p>
+        <p className="text-base md:text-xl">
+          Chosis ton style. Choisis Sneakhearts.
+        </p>
       </div>
     </div>
   );
