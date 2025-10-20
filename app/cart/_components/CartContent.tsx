b8ff80bd6a411ce6a5f4310de8168ca27a824828
ClementSellyPro <clementselly.pro@gmail.com>
2025-10-20 08:27:40 +0400

    Adapt CardContent for mobile screen

--- app/cart/_components/CartContent.tsx
+++ app/cart/_components/CartContent.tsx
@@ -57,8 +57,8 @@ export default function CartContent({ cartListData }: CartContentProps) {
   if (!cartData) return <div>Chargement...</div>;
 
   return (
-    <div className="flex justify-between min-h-screen px-52 py-18">
-      <div className="flex flex-col gap-12 w-7/12">
+    <div className="flex flex-col md:flex-row justify-between gap-18 min-h-screen px-6 md:px-24 lg:px-48 py-12 md:py-18 w-full">
+      <div className="flex flex-col gap-12 w-full md:w-7/12">
         <h1 className="text-3xl font-semibold text-center">VOTRE PANIER:</h1>
         <div className="flex flex-col gap-4">
           {cartData.cartItems.length > 0 ? (
@@ -75,7 +75,7 @@ export default function CartContent({ cartListData }: CartContentProps) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-8 w-4/12">
+      <div className="flex flex-col gap-8 w-full md:w-4/12">
         <h2 className="text-xl font-semibold">Récapitulatif</h2>
 
         {cartData.itemCount > 0 ? (
